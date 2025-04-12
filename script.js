8f46085f24ee40d515d4a0e560c605c9fcf08f0f
fivan <fivan841@gmail.com>
2025-04-12 17:07:12 +0300

    init

---
+++ script.js
@@ -0,0 +1,46 @@
+// setLineColor(100, 100, 0);
+// setLineWidth(3);
+// drawCircle(500, 150, 150);
+
+const pixelSize = 20;
+const pixelCountX = Math.floor(canvasWidth / pixelSize);
+const pixelCountY = Math.floor(canvasHeight / pixelSize);
+
+console.log(pixelCountX, pixelCountY);
+
+const drawPixel = (x, y) => {
+  drawRect(x * pixelSize + 1, y * pixelSize + 1, pixelSize - 2, pixelSize - 2);
+};
+
+// for (let i = 0; i < pixelCountX; i++) {
+//   drawPixel(i, 0);
+// }
+
+const ballCoords = {
+  x: 10,
+  y: 10,
+};
+
+const ballVelocity = {
+  x: 1,
+  y: 1,
+};
+
+const onInterval = () => {
+  clear();
+
+  ballCoords.x += ballVelocity.x;
+  ballCoords.y += ballVelocity.y;
+
+  if (ballCoords.x >= pixelCountX || ballCoords.x <= 0) {
+    ballVelocity.x = -ballVelocity.x;
+  }
+
+  if (ballCoords.y >= pixelCountY || ballCoords.y <= 0) {
+    ballVelocity.y = -ballVelocity.y;
+  }
+
+  drawPixel(ballCoords.x, ballCoords.y);
+};
+
+setInterval(onInterval, 100);
